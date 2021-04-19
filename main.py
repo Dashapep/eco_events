9cf0cac057a79e44c5aa010af455538145cf9ffe
Dashapep <pds032005@gmail.com>
2021-04-19 21:38:28 +0300

    попытка подключить heroku

--- main.py
+++ main.py
@@ -303,6 +303,6 @@ def main():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='127.0.0.1', port=port)
     main()
